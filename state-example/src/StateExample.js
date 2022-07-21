14d8fdbb98cc58c2fc95469bda92b36d296fa322
divyapriya.raghupathy <divya.raghupathy@rivetlogic.com>
2022-07-21 15:12:14 -0500

    Updated counter state value access

--- state-example/src/StateExample.js
+++ state-example/src/StateExample.js
@@ -8,18 +8,28 @@ class StateExample extends Component{
             counter: 0
         };
     }
-
+    //Set State exammple with callback
     incrementCounter(){
-        this.setState({
-            counter: this.state.counter+1
-        })
+        //Setstate method is called asynchronizly by react. 
+        this.setState(
+            {counter: this.state.counter+1}, 
+            () => {console.log("Incremeted the counter. New value of counter is " , this.state.counter)} //A call back method (an arrow function () => {}) passed as a parameter to this.setState function
+        )
     }
-
+    //Set State example without callback
     resetCounter(){
         this.setState({
             counter: 0
         });
     }
+
+    //Set State example with a previous state
+    anotherIncrementExample(number){
+        this.setState( (prevState, props) => ({counter: prevState.counter+ number})
+        );
+    }
+
+    
     render(){
         return (
             <div>
@@ -28,6 +38,7 @@ class StateExample extends Component{
                 <div className="box"> 
                     <button className="counter-buttons" onClick = { () => this.incrementCounter()} > Increment Counter </button> 
                     <button  className="counter-buttons" onClick = { () => this.resetCounter()} > Reset Counter </button>
+                    <button  className="counter-buttons" onClick = { () => this.anotherIncrementExample(5)} > Increment By 5 </button>
                 </div>
 
             </div>
