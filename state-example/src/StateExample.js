d0af5d1510b2417f7cc6b6fdfa2dff9e86465d90
divyapriya.raghupathy <divya.raghupathy@rivetlogic.com>
2022-07-21 12:51:36 -0500

    React State example code

---
+++ state-example/src/StateExample.js
@@ -0,0 +1,40 @@
+import { toHaveAccessibleDescription } from '@testing-library/jest-dom/dist/matchers';
+import React, { Component } from 'react';
+import './StateExample.css';
+
+class StateExample extends Component{
+    constructor(){
+        super();
+        this.state = {
+            counter: 0
+        };
+    }
+
+    incrementCounter(curState){
+        this.setState({
+            counter: curState.counter+1
+        })
+    }
+
+    resetCounter(){
+        this.setState({
+            counter: 0
+        });
+    }
+    render(){
+        return (
+            <div>
+                <header> State in a Class Component - Example </header>
+                <div className="box counter-header">Counter value {this.state.counter} </div>
+                <div className="box"> 
+                    <button className="counter-buttons" onClick = { () => this.incrementCounter(this.state)} > Increment Counter </button> 
+                    <button  className="counter-buttons" onClick = { () => this.resetCounter()} > Reset Counter </button>
+                </div>
+
+            </div>
+        
+        )
+    }
+}
+
+export default StateExample;
